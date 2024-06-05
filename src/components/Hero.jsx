d5126921a680f3a85fbc7ad3927f2a1b8a4624b0
chhatrapati <chhatrapati.chauhan@squareboat.com>
2024-06-05 18:29:57 +0530

    FIX : motion zitter issue

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ import Palm from "/assets/palm.png";
 const Hero = () => {
   return (
     <div className="min-h-[90vh]">
-      <div className="flex flex-col md:flex-row gap-12 min-h-[73vh] justify-center md:justify-around items-center md:px-4 md:p-2 mt-16 md:m-0">
+      <div className="flex flex-col md:flex-row gap-12 md:min-h-[73vh] min-h-[70vh] justify-center md:justify-around items-center md:px-4 md:p-2">
         <div className="md:w-1/2 flex justify-center items-center">
           <motion.img
             src={HeroImg}
@@ -15,22 +15,26 @@ const Hero = () => {
             initial={{ x: "-150%", opacity: 0 }}
             animate={{ x: 0, opacity: 1 }}
             transition={{ duration: 1, ease: "easeOut" }}
+            style={{ willChange: "transform, opacity" }}
+            loading="lazy"
           />
         </div>
         <motion.div
           initial={{ x: "150%", opacity: 0 }}
           animate={{ x: 0, opacity: 1 }}
           transition={{ duration: 1, ease: "easeOut" }}
+          style={{ willChange: "transform, opacity" }}
           className="flex flex-col items-start md:gap-2 gap-3 md:w-1/2 w-10/12 md:p-0 text-center"
         >
           <div className="flex items-center gap-4 w-full md:justify-start justify-center">
-            <h2 className="md:text-[3rem] md:text-[rgb(65, 61, 61)]  text-gray-700 whitespace-nowrap text-3xl title">
+            <h2 className="md:text-[3rem] md:text-[rgb(65, 61, 61)] text-gray-700 whitespace-nowrap text-3xl title">
               Frontend Engineer
             </h2>
             <img
               className="w-10 h-10 md:w-11 md:h-11 md:block hidden"
               src={Palm}
               alt="Palm"
+              loading="lazy"
             />
           </div>
           <div className="flex items-center gap-2 md:justify-start justify-center w-full">
@@ -42,10 +46,12 @@ const Hero = () => {
             Hello, I&apos;m Chhatrapati Chauhan, a skilled Front-end Engineer.
             Specializing in creating visually appealing and user-friendly
             websites, I focus on delivering seamless and engaging user
-            experiences. With a keen eye for detail and a passion for
-            excellence, I transform creative concepts into high-performing web
-            applications. Let&apos;s collaborate to create exceptional digital
-            experiences.
+            experiences.{" "}
+            <p className="md:block hidden">
+              With a keen eye for detail and a passion for excellence, I
+              transform creative concepts into high-performing web applications.
+              Let&apos;s collaborate to create exceptional digital experiences.
+            </p>
           </p>
         </motion.div>
       </div>
