281cfd4bc235c572bd81e9103df2d621a44341e9
chhatrapati <chhatrapati.chauhan@squareboat.com>
2024-06-05 12:18:54 +0530

    UPDATE : Hero img

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,12 +1,18 @@
 // import { Icon } from "@iconify/react/dist/iconify.js";
 import Skills from "./Skills";
-import HeroImg from "/assets/body_img.png";
+import HeroImg from "/assets/body_img2.png";
 import Palm from "/assets/palm.png";
 const Hero = () => {
   return (
     <div className=" min-h-[90vh] ">
       <div className="flex flex-col md:flex-row gap-12 min-h-[73vh] justify-center md:justify-around items-center  md:px-4 md:p-2 ">
-        <img src={HeroImg} alt="" className="h-72 w-72 md:h-96 md:w-96" />
+        <div className="md:w-1/2 flex justify-center items-center">
+          <img
+            src={HeroImg}
+            alt=""
+            className="h-56 w-h-56 md:h-[22rem] md:w-[22rem]"
+          />
+        </div>
         <div className="flex flex-col items-start md:gap-2 gap-3 md:w-1/2 w-10/12 md:p-0 text-center">
           <div className="flex items-center gap-4 w-full md:justify-start justify-center ">
             <h2 className=" md:text-[3rem] whitespace-nowrap text-3xl title">
@@ -31,10 +37,10 @@ const Hero = () => {
             </p>
           </div>
           <p className="mt-4 text-balance text-start">
-            Hi, I&apos;m Chhatrapati Chauhan. A passionate Front-end React
-            Developer based in Gurugram, India. I love crafting designs that
-            enhance the overall user experience, making it enjoyable and
-            memorable for everyone who interacts with the websites I build.
+            Hi, I&apos;m Chhatrapati Chauhan. A passionate Front-end engineer
+            based in Gurugram, India. I love crafting designs that enhance the
+            overall user experience, making it enjoyable and memorable for
+            everyone who interacts with the websites I build.
           </p>
         </div>
       </div>
